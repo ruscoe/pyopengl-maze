a2a19ee1b07f1091663f886ef4699e5c7c97b1a1
ruscoe <danruscoe@gmail.com>
2020-12-07 19:33:24 -0800

    Allow cube textures to scale.

--- src/cube.py
+++ src/cube.py
@@ -5,12 +5,16 @@ from PIL.Image import open
 
 class Cube:
 
-    def drawcube(self, texture_id = None):
+    def drawcube(self, texture_id = None, texture_tile = 1.0):
 
         if texture_id is not None:
             glEnable(GL_TEXTURE_2D)
             glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
             glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+            # Repeat the texture.
+            glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+            glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
             glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL)
 
             glBindTexture(GL_TEXTURE_2D, texture_id)
@@ -19,29 +23,29 @@ class Cube:
 
         # Textured cube.
         glTexCoord2f(0.0, 0.0); glVertex3f(-1.0, -1.0,  1.0);
-        glTexCoord2f(1.0, 0.0); glVertex3f( 1.0, -1.0,  1.0);
-        glTexCoord2f(1.0, 1.0); glVertex3f( 1.0,  1.0,  1.0);
-        glTexCoord2f(0.0, 1.0); glVertex3f(-1.0,  1.0,  1.0);
-        glTexCoord2f(1.0, 0.0); glVertex3f(-1.0, -1.0, -1.0);
-        glTexCoord2f(1.0, 1.0); glVertex3f(-1.0,  1.0, -1.0);
-        glTexCoord2f(0.0, 1.0); glVertex3f( 1.0,  1.0, -1.0);
+        glTexCoord2f(texture_tile, 0.0); glVertex3f( 1.0, -1.0,  1.0);
+        glTexCoord2f(texture_tile, texture_tile); glVertex3f( 1.0,  1.0,  1.0);
+        glTexCoord2f(0.0, texture_tile); glVertex3f(-1.0,  1.0,  1.0);
+        glTexCoord2f(texture_tile, 0.0); glVertex3f(-1.0, -1.0, -1.0);
+        glTexCoord2f(texture_tile, texture_tile); glVertex3f(-1.0,  1.0, -1.0);
+        glTexCoord2f(0.0, texture_tile); glVertex3f( 1.0,  1.0, -1.0);
         glTexCoord2f(0.0, 0.0); glVertex3f( 1.0, -1.0, -1.0);
-        glTexCoord2f(0.0, 1.0); glVertex3f(-1.0,  1.0, -1.0);
+        glTexCoord2f(0.0, texture_tile); glVertex3f(-1.0,  1.0, -1.0);
         glTexCoord2f(0.0, 0.0); glVertex3f(-1.0,  1.0,  1.0);
-        glTexCoord2f(1.0, 0.0); glVertex3f( 1.0,  1.0,  1.0);
-        glTexCoord2f(1.0, 1.0); glVertex3f( 1.0,  1.0, -1.0);
-        glTexCoord2f(1.0, 1.0); glVertex3f(-1.0, -1.0, -1.0);
-        glTexCoord2f(0.0, 1.0); glVertex3f( 1.0, -1.0, -1.0);
+        glTexCoord2f(texture_tile, 0.0); glVertex3f( 1.0,  1.0,  1.0);
+        glTexCoord2f(texture_tile, texture_tile); glVertex3f( 1.0,  1.0, -1.0);
+        glTexCoord2f(texture_tile, texture_tile); glVertex3f(-1.0, -1.0, -1.0);
+        glTexCoord2f(0.0, texture_tile); glVertex3f( 1.0, -1.0, -1.0);
         glTexCoord2f(0.0, 0.0); glVertex3f( 1.0, -1.0,  1.0);
-        glTexCoord2f(1.0, 0.0); glVertex3f(-1.0, -1.0,  1.0);
-        glTexCoord2f(1.0, 0.0); glVertex3f( 1.0, -1.0, -1.0);
-        glTexCoord2f(1.0, 1.0); glVertex3f( 1.0,  1.0, -1.0);
-        glTexCoord2f(0.0, 1.0); glVertex3f( 1.0,  1.0,  1.0);
+        glTexCoord2f(texture_tile, 0.0); glVertex3f(-1.0, -1.0,  1.0);
+        glTexCoord2f(texture_tile, 0.0); glVertex3f( 1.0, -1.0, -1.0);
+        glTexCoord2f(texture_tile, texture_tile); glVertex3f( 1.0,  1.0, -1.0);
+        glTexCoord2f(0.0, texture_tile); glVertex3f( 1.0,  1.0,  1.0);
         glTexCoord2f(0.0, 0.0); glVertex3f( 1.0, -1.0,  1.0);
         glTexCoord2f(0.0, 0.0); glVertex3f(-1.0, -1.0, -1.0);
-        glTexCoord2f(1.0, 0.0); glVertex3f(-1.0, -1.0,  1.0);
-        glTexCoord2f(1.0, 1.0); glVertex3f(-1.0,  1.0,  1.0);
-        glTexCoord2f(0.0, 1.0); glVertex3f(-1.0,  1.0, -1.0);
+        glTexCoord2f(texture_tile, 0.0); glVertex3f(-1.0, -1.0,  1.0);
+        glTexCoord2f(texture_tile, texture_tile); glVertex3f(-1.0,  1.0,  1.0);
+        glTexCoord2f(0.0, texture_tile); glVertex3f(-1.0,  1.0, -1.0);
 
         # Colored cube.
         #
