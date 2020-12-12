148957305f6bafd7f574384dc64bbf983029a969
ruscoe <danruscoe@gmail.com>
2020-12-12 13:23:43 -0800

    Update map generator to create both vertical and horizontal walls.

--- src/generator.py
+++ src/generator.py
@@ -3,7 +3,7 @@ import numpy
 
 class Generator:
 
-    def generateMap(self, size = 16, rows = 3, columns = 3):
+    def generateMap(self, size = 16, columns_as_walls = 3, rows_as_walls = 3):
 
         #
         rows = [0] * size
@@ -19,20 +19,41 @@ class Generator:
 
         # Determine which columns will be walls.
         # Example: [4, 11, 7]
-        sample = random.sample(list(map_range), columns)
+        sample = random.sample(list(map_range), columns_as_walls)
 
         # Mark columns as walls.
-        for i in sample:
-            first_row[i] = 1
+        for column_id in sample:
+            first_row[column_id] = 1
 
         # Duplicate the first row until the full map size is met.
         for i in range(size):
-            rows[i] = first_row
+            rows[i] = first_row.copy()
 
-        # TODO: Turn some rows into walls.
+        # Remove one segment from each column to create pathways.
+        for column_id in sample:
+            row_to_remove = random.choice(list(map_range))
+            rows[row_to_remove][column_id] = 0
 
+        # Determine which rows will be walls.
+        # Example: [4, 11, 7]
+        sample = random.sample(list(map_range), rows_as_walls)
+
+        # Mark rows as walls.
+        for row_id in sample:
+            for i in range(size):
+                rows[row_id][i] = 1
+
+        # Remove one segment from each row to create pathways.
+        for row_id in sample:
+            column_to_remove = random.choice(list(map_range))
+            rows[row_id][column_to_remove] = 0
+
+        # Uncomment to print map as a grid.
+        # for i in range(size):
+        #     print(rows[i])
 
         # Hard-coded map for reference.
+        # Uncomment to manually create a map.
         #
         # 1 = wall
         # 0 = path
