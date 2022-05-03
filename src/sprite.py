87521138f6eb6aaf1d4f8e78278ce6af1629daee
ruscoe <danruscoe@gmail.com>
2022-05-02 22:03:48 -0700

    Remove code to repeat texture on sprite.

--- src/sprite.py
+++ src/sprite.py
@@ -11,12 +11,7 @@ class Sprite:
             glEnable(GL_TEXTURE_2D)
             glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
             glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
-            # Repeat the texture.
-            glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-            glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
             glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL)
-
             glBindTexture(GL_TEXTURE_2D, texture_id)
 
         glBegin(GL_QUADS)
